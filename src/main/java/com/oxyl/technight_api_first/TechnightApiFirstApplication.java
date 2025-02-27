96fa2e9101b5a86f65c91c57ba8925d78d506596
fguibon <49637288+fguibon@users.noreply.github.com>
2025-02-27 10:24:09 +0100

    feat: init spring boot app

---
+++ src/main/java/com/oxyl/technight_api_first/TechnightApiFirstApplication.java
@@ -0,0 +1,13 @@
+package com.oxyl.technight_api_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnightApiFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnightApiFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oxyl/technight_api_first/TechnightApiFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oxyl.technight_api_first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnightApiFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
