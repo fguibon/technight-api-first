317623eb53f7277dc2e0334dae13c088cea31212
fguibon <49637288+fguibon@users.noreply.github.com>
2025-03-04 11:28:35 +0100

    feat: add bad request exception handling

--- src/main/java/com/oxyl/technight_api_first/config/GlobalControllerAdvice.java
+++ src/main/java/com/oxyl/technight_api_first/config/GlobalControllerAdvice.java
@@ -5,11 +5,17 @@ import feign.FeignException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 
 @RestControllerAdvice
 public class GlobalControllerAdvice {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ErrorDto> handle(ResponseStatusException exception) {
+        return ResponseEntity.badRequest().body(toErrorDto(exception.getReason()));
+    }
 
     @ExceptionHandler({FeignException.class, IOException.class})
     public ResponseEntity<ErrorDto> handleClientException() {
@@ -18,7 +24,7 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDto> handle() {
-        return ResponseEntity.badRequest().body(toErrorDto("An error occurred, contact the administrator"));
+        return ResponseEntity.internalServerError().body(toErrorDto("An error occurred, contact the administrator"));
     }
 
     private ErrorDto toErrorDto(String message) {
--- src/main/java/com/oxyl/technight_api_first/controller/MealController.java
+++ src/main/java/com/oxyl/technight_api_first/controller/MealController.java
@@ -5,8 +5,10 @@ import com.oxyl.technight_api_first.server.api.MealsApi;
 import com.oxyl.technight_api_first.server.model.MealListDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class MealController implements MealsApi {
@@ -21,7 +23,7 @@ public class MealController implements MealsApi {
     public ResponseEntity<MealListDto> getMeals(String ingredient, String category, String area) {
         logger.info("[getMeals] {} {} {}", ingredient, category, area);
         if (ingredient == null && category == null && area == null) {
-            return ResponseEntity.ok(new MealListDto());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "One parameter must be completed", null);
         }
         return ResponseEntity.ok(mealService.getMeals(ingredient, category, area));
     }
