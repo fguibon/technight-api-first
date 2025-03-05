f2d5baac6ae3c8b7f64da2b5c1207570a8294194
fguibon <49637288+fguibon@users.noreply.github.com>
2025-03-05 15:55:27 +0100

    feat: add logs

--- src/main/java/com/oxyl/technight_api_first/config/GlobalControllerAdvice.java
+++ src/main/java/com/oxyl/technight_api_first/config/GlobalControllerAdvice.java
@@ -2,6 +2,8 @@ package com.oxyl.technight_api_first.config;
 
 import com.oxyl.technight_api_first.server.model.ErrorDto;
 import feign.FeignException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,21 +11,30 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 @RestControllerAdvice
 public class GlobalControllerAdvice {
+    private final Logger logger = LoggerFactory.getLogger(GlobalControllerAdvice.class);
 
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<ErrorDto> handle(ResponseStatusException exception) {
-        return ResponseEntity.badRequest().body(toErrorDto(exception.getReason()));
+        logger.error(exception.getReason());
+        return switch (exception.getStatusCode()) {
+            case BAD_REQUEST -> ResponseEntity.badRequest().body(toErrorDto(exception.getReason()));
+            default -> ResponseEntity.internalServerError().body(toErrorDto(exception.getReason()));
+        };
     }
 
     @ExceptionHandler({FeignException.class, IOException.class})
-    public ResponseEntity<ErrorDto> handleClientException() {
+    public ResponseEntity<ErrorDto> handleClientException(Exception e) {
+        logger.error("An client exception occurred {}", e.getMessage());
         return ResponseEntity.internalServerError().body(toErrorDto("Error trying to reach client api, try again"));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorDto> handle() {
+    public ResponseEntity<ErrorDto> handle(Exception e) {
+        logger.error("An exception occurred {}", e.getMessage());
         return ResponseEntity.internalServerError().body(toErrorDto("An error occurred, contact the administrator"));
     }
 
